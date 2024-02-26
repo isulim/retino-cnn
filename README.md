63ad58d96cf59ea680b687a6400afe2e46ac4ca9
isulim <igosulim@gmail.com>
2024-02-26 20:44:52 +0100

    Add option to not unzip files

--- README.md
+++ README.md
@@ -1,10 +1,20 @@
 # brain-cnn
 An app using CNN to determine weather cancer is/not present in given brain CT scan.
 
+## Dataset
+
 ### About dataset
 `Brain Tumor Dataset` from Kaggle: [https://www.kaggle.com/datasets/preetviradiya/brian-tumor-dataset](https://www.kaggle.com/datasets/preetviradiya/brian-tumor-dataset)  
 - 4602 images, including 2513 tumor and 2087 healthy scans
 
-To download the dataset, please create a personal Kaggle API token under Settings -> Account -> API:  
-[https://www.kaggle.com/settings](https://www.kaggle.com/settings)  
-and store it as `${KAGGLE_CONFIG_DIR}/kaggle.json` in project root directory (`~/.kaggle/kaggle.json` by default).
+
+### Download dataset
+To download the dataset, first create a personal Kaggle API token under  
+`Settings -> Account -> API`  [https://www.kaggle.com/settings](https://www.kaggle.com/settings)  
+and store it in `${KAGGLE_CONFIG_DIR}/kaggle.json` directory (or `~/.kaggle/kaggle.json` which is default for Kaggle API).
+
+Next:
+```
+make download-dataset # Download dataset from Kaggle (with optional unzipping)
+make rename-files # Rename files to remove whitespaces and parentheses
+```
--- src/data/download_data.py
+++ src/data/download_data.py
@@ -1,7 +1,31 @@
 import os
+import zipfile
 
 from dotenv import load_dotenv
 load_dotenv()
+
+
+def download_dataset(dataset):
+    api.authenticate()
+    api.dataset_download_files(dataset=dataset, path=path, quiet=False, unzip=False)
+
+
+def unzip_dataset(api, dataset, path):
+    try:
+        outfile = api.split_dataset_string(dataset)[1]
+        with zipfile.ZipFile(f"{path}/{outfile}.zip") as z:
+            z.extractall(path)
+        print('Unzipped all files.')
+
+        os.remove(f"{path}/{outfile}.zip")
+        print('Deleted zip file.')
+
+    except zipfile.BadZipFile as e:
+        raise ValueError(
+            'Bad zip file, please report on '
+            'www.github.com/kaggle/kaggle-api', e)
+    except FileNotFoundError as e:
+        print(e)
 
 
 if __name__ == "__main__":
@@ -9,7 +33,12 @@ if __name__ == "__main__":
     os.makedirs(path, exist_ok=True)
 
     dataset = os.getenv("KAGGLE_DATASET")
-
     from kaggle import api
-    api.authenticate()
-    api.dataset_download_files(dataset=dataset, path=path, quiet=False, unzip=True)
+
+    download_dataset(dataset)
+
+    unzip = input("Do you want to unzip the files? (Y/n): ")
+    if unzip.lower() == "y":
+        unzip_dataset(api, dataset, path)
+    else:
+        print("Files downloaded successfully")
