effe06b64dcef5e3225ec1e3c9e55e1dc93df0f5
isulim <igosulim@gmail.com>
2024-03-12 20:44:27 +0100

    Change rename_files.py

--- src/data/rename_files.py
+++ src/data/rename_files.py
@@ -1,6 +1,5 @@
 """
-Rename filenames to lowercase and replace spaces with underscores.
-Move the directories one up.
+Remove unused CSV files and one directory with unused images.
 """
 
 import os
@@ -9,31 +8,45 @@ import shutil
 from pathlib import Path
 
 from dotenv import load_dotenv
+from tqdm import tqdm
+
+
+def remove_unused_files(path: Path):
+    """Remove unused CSV files and one directory with unused images."""
+
+    train_labels19 = Path(path, "trainLabels19.csv")
+    train_labels15 = Path(path, "trainLabels15.csv")
+    test_labels15 = Path(path, "testLabels15.csv")
+
+    for file in tqdm([train_labels19, train_labels15, test_labels15]):
+        if file.exists():
+            os.remove(file)
+            print(f"Removed {file}")
+
+    imgs = Path(path, 'resized_test19')
+    if imgs.exists():
+        shutil.rmtree(imgs)
+        print(f"Removed {imgs} directory")
 
 
 def rename_files(path: Path):
-    for root, dirs, files in path.walk():
-        for file in files:
-            filename = file.replace(" ", "_").replace("(", "").replace(")", "").replace("__", "_").lower()
-            if file != filename:
-                os.rename(f"{path}/{file}", f"{path}/{filename}")
-                print(f"Renamed {file} to {filename}")
+    """Rename files in the directory to match the format of the labels CSV file."""
 
-        for dir in dirs:
-            rename_files(Path(f"{path}/{dir}"))
-            dirname = dir.replace(" ", "_").replace("(", "").replace(")", "").lower()
-            if dir != dirname:
-                os.rename(f"{path}/{dir}", f"{path}/{dirname}")
-                print(f"Renamed {dir} to {dirname}")
+    images = Path(path, 'resized_traintest15_train19')
+    shutil.move(images, Path(path, 'images'))
+    print(f"Renamed `raw/resized_traintest15_train19` to `raw/images`")
+    labels = Path(path, 'labels', 'traintestLabels15_trainLabels19.csv')
+    shutil.move(labels, Path(path, 'labels.csv'))
+    shutil.rmtree(Path(path, "labels"))
+    print(f"Renamed `raw/labels/traintestLabels15_trainLabels19` to raw/labels.csv")
 
 
 if __name__ == "__main__":
+
     load_dotenv()
     kaggle_dir: str = os.getenv("KAGGLE_FILES_DIR", "")
     raw_path = Path(kaggle_dir, "raw")
+
+    remove_unused_files(raw_path)
     rename_files(raw_path)
 
-    # Move the directories one up
-    shutil.move(Path(raw_path, "brain_tumor_data_set", "brain_tumor_data_set", "brain_tumor"), Path(raw_path, "tumor"))
-    shutil.move(Path(raw_path, "brain_tumor_data_set", "brain_tumor_data_set", "healthy"), Path(raw_path, "healthy"))
-    shutil.rmtree(Path(raw_path, "brain_tumor_data_set"))
