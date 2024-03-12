41233218ddcdc7184e85bba3335db9cbef496bb8
isulim <igosulim@gmail.com>
2024-03-12 20:56:22 +0100

    Split files into train, validation and test directories

--- src/data/train_val_test_split.py
+++ src/data/train_val_test_split.py
@@ -1,19 +1,21 @@
 """Split folders with images into train and validation sets using `split-folders`."""
 
 import os
+
 from pathlib import Path
 
-from splitfolders import ratio  # type: ignore
 from dotenv import load_dotenv
+from splitfolders import ratio  # type: ignore
+
 
 if __name__ == "__main__":
 
     load_dotenv()
     seed = os.getenv("SEED")
-    kaggle_dir = os.getenv("KAGGLE_FILES_DIR", "")
-    source_path = Path(kaggle_dir, "raw")
-    output_path = Path(kaggle_dir, "processed")
-    os.makedirs(output_path, exist_ok=True)
+    kaggle_dir: str = os.getenv("KAGGLE_FILES_DIR", "")
 
-    ratio(source_path, output=output_path, seed=seed, ratio=(0.7, 0.2, 0.1))
+    raw_path = Path(kaggle_dir, "raw")
+    processed_path = Path(kaggle_dir, "processed")
+
     print("Splitting folders into train, validation, test sets.")
+    ratio(raw_path, output=processed_path, seed=seed, ratio=(0.7, 0.2, 0.1))
