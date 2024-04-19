f44d212005a3c3405673e5fd74114abc2fea65f5
Igor Sulim <igosulim@gmail.com>
2024-04-19 22:14:05 +0200

    Change name of script

--- Makefile
+++ Makefile
@@ -1,6 +1,8 @@
-all: input-env download-data rename-files split-data
+all: configure-envs download-data rename-files split-data
 
-process-data: download-data rename-files split-data
+download-and-process: download-data rename-files split-data
+
+process-data: rename-files split-data
 
 download-rename-data: download-data rename-files
 
@@ -13,5 +15,5 @@ rename-files:
 download-data:
 	poetry run python scripts/data/download_data.py
 
-input-env:
-	poetry run python scripts/data/input_env.py
+configure-envs:
+	poetry run python scripts/data/configure_envs.py
--- scripts/data/configure_envs.py
+++ scripts/data/configure_envs.py
@@ -1,4 +1,4 @@
-"""Input environment variables."""
+"""Configure environment variables."""
 
 
 def ask_kaggle_config():
@@ -21,26 +21,19 @@ def ask_files_dir():
     return files_dir
 
 
-def ask_kaggle_dataset():
-    """Ask for Kaggle dataset name."""
-    dataset = input("Enter Kaggle dataset name: ")
-    if not dataset:
-        raise ValueError("Please enter a valid dataset name.")
-    return dataset
 
-
-def confirm(files_dir: str, dataset: str, kaggle_dir: str):
+def confirm(files_dir: str, kaggle_dir: str):
     """Confirm input."""
+
     confirm = input(f"""Is provided information correct?
     DATA_FILES_DIR: {files_dir}
     KAGGLE_CONFIG_DIR: {kaggle_dir}
-    KAGGLE_DATASET: {dataset}
     (Y/n): """)
     if not confirm or confirm.lower() == "y":
         with open(".env", "a") as f:
             f.write(f"KAGGLE_FILES_DIR={files_dir}\n")
             f.write(f"KAGGLE_CONFIG_DIR={kaggle_dir}\n")
-            f.write(f"KAGGLE_DATASET={dataset}\n")
+            f.write(f"KAGGLE_DATASET=c7934597/resized-2015-2019-diabetic-retinopathy-detection\n")
     else:
         print("Please re-enter the information.")
 
@@ -48,5 +41,4 @@ def confirm(files_dir: str, dataset: str, kaggle_dir: str):
 if __name__ == '__main__':
     kaggle_dir = ask_kaggle_config()
     files_dir = ask_files_dir()
-    dataset = ask_kaggle_dataset()
-    confirm(files_dir, dataset, kaggle_dir)
+    confirm(files_dir, kaggle_dir)
